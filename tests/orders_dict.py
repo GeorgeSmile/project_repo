81d4079fcaae4701b291a443a441976dde40bbd8
George <george78090@gmail.com>
2022-11-10 14:52:45 +0000

    Third commit: modified 'testing_week2' by adding basket menu. Added basket.txt, couriers.txt and orders_dict. Couriers and orders will be modified in future. basket.txt is mutable by the user and the menu for it will be where orders can be placed and confirmed.

---
+++ tests/orders_dict.py
@@ -0,0 +1,8 @@
+customer = {
+    "customer_name": "John",
+    "customer_address": "Unit 2, 12 Main Street, LONDON, WH1 2ER",
+    "customer_phone": "0789887334",
+    "courier": 2,
+    "status": "perparing"
+
+}
