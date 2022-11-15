3f7cf2cf506b05bcfbfa058b002cd2ab88bab841
George <george78090@gmail.com>
2022-11-15 13:32:37 +0000

    Adding changes to orders_dict. USer input when this is executed gets added to Orders.txt in a dictionary format. For storing user/customer address and contact information.

--- tests/orders_dict.py
+++ tests/orders_dict.py
@@ -1,8 +1,26 @@
-customer = {
-    "customer_name": "John",
-    "customer_address": "Unit 2, 12 Main Street, LONDON, WH1 2ER",
-    "customer_phone": "0789887334",
-    "courier": 2,
-    "status": "perparing"
+## Adds user input to Orders.txt file in dictionary format.
+## Each piece of data is added to a new line.
 
-}
+import json
+
+
+
+customer = {}
+
+for i in range(1):
+    customer_name = input("Enter your name: ")
+    customer_address = input("Enter your address: ")
+    customer_phone = input("Enter your phone number: ")
+
+    customer["customer_name"] = customer_name
+    customer["customer_address"] = customer_address
+    customer["customer_phone"] = customer_phone
+        
+    
+    
+
+
+    print(customer)
+
+    with open('C:/vscode/Python/Projects/mini_project/tests/Orders.txt', 'a+') as file: 
+            file.write(json.dumps(customer, indent=2))
