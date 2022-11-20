1e05626aeda8f9069b98b9bfd88db5e385684a2f
George <george78090@gmail.com>
2022-11-20 15:29:02 +0000

    Resolved issue with choosing product index numbers when placing an order looping incorrectly and printing to orders_dict incorrectly. Index numbers for products still not showing during this loop. Also fully switched from .txt. to .csv. Used pandas for updating order status on orders_dict.csv

--- tests/main.py
+++ tests/main.py
@@ -1,6 +1,8 @@
 import json
 import csv
 from operator import index
+import pandas as pd
+from sys import exit
 
 
 def main():    
@@ -10,130 +12,84 @@ def main():
     
     Main menu:
     [1] Products menu
-    [2] Create new product
+    [2] Add new product
     [3] Remove product
-    [4] Basket
-    [5] Place order menu
+    [4] Place order menu
     [0] Exit
     
     ''')
 
         if options == "0":
             print("Exiting...")
-            break
+            exit()
         
         elif options == "1":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/drinks.txt", 'r')
-            lines = file.readlines()
-            for i, line in enumerate(lines, start=1):
-                print(f"{i}  {line.strip()}")
+            print("\nList of products:")
+            df = pd.read_csv("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv")
+            print(df)
+            input("\nPress enter to return to main menu.")
+            main()
             
-                
         elif options == "2":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/drinks.txt", 'a+')
-            while True:
-                new_product = input("Enter new product name, or type 'exit' to return to menu: ")
-                if new_product == "exit":
-                    break
-                file.write(new_product + "\n")
+            df = pd.read_csv("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv")
+            print(df)
+            new_pname = input("Please enter the new product name or type exit to return to previous menu: ")
+            if new_pname == "exit":
+                main()
+            else:
+                new_pprice = input("Please enter the new product price: ")
+                new_product = [new_pname, float(new_pprice)]
+                print(new_product)
+                df.loc[-1] = new_product  # type: ignore
+                df = df.reset_index(drop = True) # resets index
+                df.to_csv("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv", index=False)
+                print(df)
+                input("Press enter to return to main menu.")
+                main()
 
         elif options == "3":
             while True:
-                print("Enter the product name to delete, or type 0 to return to menu: ")
-                line_text = input()
-                file_handle = open('C:/vscode/Python/Projects/mini_project/tests/drinks.txt', "r")
-                lines = file_handle.readlines()
-                file_handle.close()
-                file_handle = open('C:/vscode/Python/Projects/mini_project/tests/drinks.txt', "w")
-                for line in lines:
-                    if line.strip("\n") != line_text:
-                        file_handle.write(line)
-                file_handle.close()
-                print("\nThe Line Deleted Successfully!")
-                if line_text == "0":
-                    break
+                print("\nList of products")
+                df = pd.read_csv("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv")
+                df.drop(df.filter(regex="Unname"),axis=1, inplace=True)  # type: ignore
+                print(df)
+                delete_product = input("Please enter the index number of the product you want to delete or type 'exit' to return to the previous menu: ")
+                if delete_product == "exit":
+                    main()
+                else:
+                    df = df.drop(labels=int(delete_product), axis = 0)
+                    df.to_csv("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv", index=False) # need index=False to see index numbers properly
+                print(df)
+                input("Press enter to return to main menu.")
+                main()
         
         elif options == "4":
-            basket()
-
-        elif options == "5":
             order_menu()
-        
-        
-def basket():
-    while True:
-        basket_options = input('''
-    
-    Basket options:
-    [1] Basket contents
-    [2] Products menu
-    [3] Add product to basket
-    [4] Remove from basket
-    [0] Return to main menu
-    
-    ''')
 
-        if basket_options == "1":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/basket.txt", 'r')
-            lines = file.readlines()
-            for i, line in enumerate(lines, start=1):
-                print(f"{i}  {line.strip()}")
-
-        elif basket_options == "2":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/drinks.txt", 'r')
-            lines = file.readlines()
-            for i, line in enumerate(lines, start=1):
-                print(f"{i}  {line.strip()}")
-        
-        elif basket_options == "3":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/basket.txt", 'a+')
-            while True:
-                new_product = input("Enter product name, or type 'exit' to return: ")
-                if new_product == "exit":
-                    break
-                file.write(new_product + "\n")
-
-        elif basket_options == "4":
-            while True:
-                print("Enter the product name to delete, or type 0 to return to basket: ")
-                line_text = input()
-                file_handle = open('C:/vscode/Python/Projects/mini_project/tests/basket.txt', "r")
-                lines = file_handle.readlines()
-                file_handle.close()
-                file_handle = open('C:/vscode/Python/Projects/mini_project/tests/basket.txt', "w")
-                for line in lines:
-                    if line.strip("\n") != line_text:
-                        file_handle.write(line)
-                file_handle.close()
-                print("\nThe Line Deleted Successfully!")
-                if line_text == "0":
-                    basket()
-        
-        elif basket_options == "0":
-            break
-        
 def order_menu():
     while True:
         order_options = input('''
     Order menu:
     [1] View couriers 
-    [2] View basket
+    [2] View orders
     [3] Place order
-    [4] 
+    [4] Update order
     [0] Return to main menu                        
                             ''')
         
         if order_options == "1":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/couriers.csv", 'r')
-            lines = file.readlines()
-            for i, line in enumerate(lines, start=1):
-                print(f"{i}  {line.strip()}")
+            file = open("C:/vscode/Python/Projects/mini_project/tests/couriers_dict.csv", 'r')
+            view_couriers = csv.DictReader(file)
+            for row in view_couriers:
+                print(dict(row))
                 
         elif order_options == "2":
-            file = open("C:/vscode/Python/Projects/mini_project/tests/basket.txt", 'r')
-            lines = file.readlines()
-            for i, line in enumerate(lines, start=1):
-                print(f"{i}  {line.strip()}")
+            print("\nList of orders:")
+            df = pd.read_csv("C:/vscode/Python/Projects/mini_project/tests/orders_dict.csv")
+            print(df)
+            input("\nPress enter to return to main menu.")
+            main()
+                
         
         elif order_options == "3":
             customer = {}
@@ -149,50 +105,56 @@ def order_menu():
                 if customer_phone == "0":
                     break
                 
-                courier = open("C:/vscode/Python/Projects/mini_project/tests/couriers.txt", 'r')
-                courier_file = courier.readlines()
-                for i in range(len(courier_file)):
-                    print(i, courier_file[i])
-                courier_index = int(input("Enter a courier index number, 0 to cancel: "))
-                if courier_index == "0":
+                courier = open("C:/vscode/Python/Projects/mini_project/tests/couriers_dict.csv", 'r')
+                reader = csv.DictReader(courier)
+                for row in reader:
+                    print(row)
+                customer_courier = int(input("Enter a courier index number, type 'exit' to cancel: "))
+                if customer_courier == "exit":
                     break
-                customer_courier = courier_file[courier_index].strip()
     
-                file = open("C:/vscode/Python/Projects/mini_project/tests/basket.txt", 'r')
-                customer_order = file.readlines()
-                customer_order_list = []
-                for i in customer_order:
-                    customer_order_list.append(i.strip())
-                    
-                status = ["Delivered", "Confirmed and pending", "Declined", "Out for Delivery"]
-
-                if not customer_courier:
-                    status = status[2]
-                else:
-                    status = status[1]
-            
-                customer["customer_name"] = customer_name
-                customer["customer_address"] = customer_address
-                customer["customer_phone"] = customer_phone
-                customer["customer_order_list"] = customer_order_list
-                customer["customer_courier"] = customer_courier
-                customer["order_status"] = status
-                
-                    
-                print(customer)
-
-                with open('C:/vscode/Python/Projects/mini_project/tests/Orders.txt', 'a+') as file: 
-                        file.write(json.dumps(customer, indent=2))
-                        print("Order placed successfully!")
-                        
+                file = open("C:/vscode/Python/Projects/mini_project/tests/drinks_dict.csv", 'r')
+                customer_order = []
+                reader = csv.DictReader(file)
+                for row in reader:
+                    print(row)
+                while True:
+                    customer_order.append(input("Enter product index number, 'n' to cancel: "))
+                    customer["customer_name"] = customer_name
+                    customer["customer_address"] = customer_address
+                    customer["customer_phone"] = customer_phone
+                    customer["customer_order"] = customer_order
+                    customer["customer_courier"] = customer_courier
+                    if customer_order[-1] == "n":
+                        customer_order.pop() # Removing 'n' from customer_order
+                        with open('C:/vscode/Python/Projects/mini_project/tests/orders_dict.csv', 'a+', newline='') as file:
+                            fieldnames = ["customer_name", "customer_address", "customer_phone", "customer_courier","customer_order"]
+                            csvwriter = csv.DictWriter(file, fieldnames=fieldnames)
+                            csvwriter.writerow(customer)
                         break
-        
-        elif order_options == "0":
-            break
-        
-            
-main()
-    
-
-    
+                        
+                    
+                    print(customer)
+                    
+                    
+        elif order_options == "4":
+            update_order_status()
                 
+        elif order_options == "0":
+            print('Returning...')
+            main()
+        
+def update_order_status():
+    df = pd.read_csv('C:/vscode/Python/Projects/mini_project/tests/orders_dict.csv')
+    print(df)
+    update_status = input("Please select the index number of the order you would like to update: ")
+    order_statuses = ["PREPARING", "SHIPPED", "DELIVERED", "CANCELLED"]
+    for i, status in enumerate(order_statuses):
+        print(i, status)
+    new_status = input("Select the new status by typing the index value above: ")
+    df.loc[int(update_status), "status"] = order_statuses[int(new_status)]
+    df.to_csv('C:/vscode/Python/Projects/mini_project/tests/orders_dict.csv', index = False)
+    input("Press enter to return to orders menu.")
+    order_menu()
+                        
+main()
